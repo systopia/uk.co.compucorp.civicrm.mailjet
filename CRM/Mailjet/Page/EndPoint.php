5f52a852aee4457cdae142211762576927092f40
scardinius <scardinius@chords.pl>
2016-04-07 17:44:35 +0200

    Add on_hold when email has hard_bounce

--- CRM/Mailjet/Page/EndPoint.php
+++ CRM/Mailjet/Page/EndPoint.php
@@ -57,23 +57,23 @@ class CRM_Mailjet_Page_EndPoint extends CRM_Core_Page {
     CRM_Core_Error::debug_var("MAILJET TRIGGER", $trigger, true, true);
     if (substr($mailingId, 0, 5) === "TRANS" || substr($mailingId, 0, 15) === "=?utf-8?Q?TRANS") {
       CRM_Core_Error::debug_var("TRANS EMAIL", array($mailingId, $event, $email), true, true);
-//      if (!in_array($event, array("bounce", "blocked"))) {
-//        return;
-//      }
       $emailResult = civicrm_api3('Email', 'get', array('email' => $email, 'sequential' => 1));
       if (isset($emailResult['values']) && !empty($emailResult['values'])) {
-        //we always get the first result
-//        $emailId = $emailResult['values'][0]['id'];
-//        civicrm_api3('Email', 'create', array(
-//          'id' => $emailId,
-//          'email' => $email,
-//          'on_hold' => true,
-//          'hold_date' => date('Y-m-d H:i:s'),
-//        ));
+        if ($event == 'bounce' && $trigger['hard_bounce']) {
+          //we always get the first result
+          $emailId = $emailResult['values'][0]['id'];
+          civicrm_api3('Email', 'create', array(
+            'id' => $emailId,
+            'email' => $email,
+            'on_hold' => true,
+            'hold_date' => date('Y-m-d H:i:s'),
+          ));  
+        }
+        
         $contactId = $emailResult['values'][0]['contact_id'];
         $params = array(
           'sequential' => 1,
-          'activity_type_id' => 58,
+          'activity_type_id' => 58, // Bounce
           'activity_date_time' => $time,
           'status_id' => 'Completed',
           'subject' => $event,
