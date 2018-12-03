bc31b786153716bfb40d2a2c495e8781990be286
scardinius <scardinius@chords.pl>
2018-12-03 13:29:49 +0100

    Remove unused variables mailingId and email

--- CRM/Mailjet/BAO/Event.php
+++ CRM/Mailjet/BAO/Event.php
@@ -2,8 +2,14 @@
 
 class CRM_Mailjet_BAO_Event extends CRM_Mailjet_DAO_Event {
 
-  static function getMailjetCustomCampaignId($jobId) {
-    if ($jobId !== null) {
+  /**
+   * @param $jobId
+   *
+   * @return string
+   * @throws \CiviCRM_API3_Exception
+   */
+  public static function getMailjetCustomCampaignId($jobId) {
+    if ($jobId !== NULL) {
       $mailingJob = civicrm_api3('MailingJob', 'get', $params = array('id' => $jobId));
       if ($mailingJob['values'][$jobId]['job_type'] == 'child') {
         $timestamp = strtotime($mailingJob['values'][$jobId]['scheduled_date']);
@@ -18,7 +24,7 @@ class CRM_Mailjet_BAO_Event extends CRM_Mailjet_DAO_Event {
    *
    * @param \CRM_Mailjet_Logic_Message $message
    */
-  static function createFromPostData(CRM_Mailjet_Logic_Message $message) {
+  public static function createFromPostData(CRM_Mailjet_Logic_Message $message) {
     $mailjetEvent = new CRM_Mailjet_DAO_Event();
     $mailjetEvent->mailing_id = $message->mailingId;
     $mailjetEvent->email = $message->email;
@@ -28,23 +34,27 @@ class CRM_Mailjet_BAO_Event extends CRM_Mailjet_DAO_Event {
     $mailjetEvent->time = $message->time;
     $mailjetEvent->data = serialize($message->trigger);
     $mailjetEvent->created_date = date('YmdHis');
-    $mailjetEvent->save(); 
+    $mailjetEvent->save();
   }
 
-  static function recordBounce($params) {
-    $isSpam =  CRM_Utils_Array::value('is_spam', $params);
-    $mailingId = CRM_Utils_Array::value('mailing_id', $params); //CiviCRM mailling ID
-    $contactId = CRM_Utils_Array::value('contact_id' , $params);
-    $emailId =  CRM_Utils_Array::value('email_id' , $params);
-    $email = CRM_Utils_Array::value('email' , $params);
-    $jobId = CRM_Utils_Array::value('job_id' , $params);
+  /**
+   * @param $params
+   *
+   * @return bool
+   * @throws \CiviCRM_API3_Exception
+   */
+  public static function recordBounce($params) {
+    $isSpam = CRM_Utils_Array::value('is_spam', $params);
+    $contactId = CRM_Utils_Array::value('contact_id', $params);
+    $emailId = CRM_Utils_Array::value('email_id', $params);
+    $jobId = CRM_Utils_Array::value('job_id', $params);
     $eqParams = array(
       'job_id' => $jobId,
       'contact_id' => $contactId,
       'email_id' => $emailId,
     );
     $eventQueue = CRM_Mailing_Event_BAO_Queue::create($eqParams);
-    $time =  date('YmdHis', CRM_Utils_Array::value('date_ts', $params));
+    $time = date('YmdHis', CRM_Utils_Array::value('date_ts', $params));
     $bounceType = array();
     // fixme deprecated function
     CRM_Core_PseudoConstant::populate($bounceType, 'CRM_Mailing_DAO_BounceType', TRUE, 'id', NULL, NULL, NULL, 'name');
@@ -60,16 +70,19 @@ class CRM_Mailjet_BAO_Event extends CRM_Mailjet_DAO_Event {
       $blocked = CRM_Utils_Array::value('blocked', $params); //  blocked : true if this bounce leads to recipient being blocked
       if ($hardBounce && $blocked) {
         $bounce->bounce_type_id = $bounceType[CRM_Mailjet_Upgrader::BLOCKED];
-      } elseif ($hardBounce && !$blocked){
+      }
+      elseif ($hardBounce && !$blocked) {
         $bounce->bounce_type_id = $bounceType[CRM_Mailjet_Upgrader::HARD_BOUNCE];
-      } else {
+      }
+      else {
         if (self::isHardError($params)) {
           $bounce->bounce_type_id = $bounceType[CRM_Mailjet_Upgrader::HARD_BOUNCE];
-        } else {
+        }
+        else {
           $bounce->bounce_type_id = $bounceType[CRM_Mailjet_Upgrader::SOFT_BOUNCE];
         }
       }
-      $bounce->bounce_reason  =  $params['error_related_to'] . " - " . $params['error'];
+      $bounce->bounce_reason = $params['error_related_to'] . " - " . $params['error'];
     }
     $bounce->save();
     if ($bounce->bounce_type_id != $bounceType[CRM_Mailjet_Upgrader::SOFT_BOUNCE]) {
@@ -98,4 +111,5 @@ class CRM_Mailjet_BAO_Event extends CRM_Mailjet_DAO_Event {
     ];
     return in_array($error, $hardErrors);
   }
+
 }
