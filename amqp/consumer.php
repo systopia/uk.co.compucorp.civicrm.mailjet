751b13dd80724c8ba1d91ecac3eecfc1b914ee10
Romain Thouvenin <romain@wemove.eu>
2019-07-11 12:12:06 +0200

    Replace AMQP steam connection with SSL connection

--- amqp/consumer.php
+++ amqp/consumer.php
@@ -17,7 +17,7 @@ require_once 'CRM/Core/Config.php';
 $civicrm_config = CRM_Core_Config::singleton();
 
 require_once __DIR__ . '/vendor/autoload.php';
-use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Connection\AMQPSSLConnection;
 
 function debug($msg) {
   echo time(), ': ', $msg, "\n";
@@ -34,9 +34,13 @@ $queue_name = $arguments['q'];
 $error_queue = $arguments['e'];
 
 function connect() {
-  return new AMQPStreamConnection(
+  return new AMQPSSLConnection(
     CIVICRM_AMQP_HOST, CIVICRM_AMQP_PORT,
-    CIVICRM_AMQP_USER, CIVICRM_AMQP_PASSWORD, CIVICRM_AMQP_VHOST);
+    CIVICRM_AMQP_USER, CIVICRM_AMQP_PASSWORD, CIVICRM_AMQP_VHOST,
+    array(
+      'local_cert' => CIVICRM_SSL_CERT,
+      'local_pk' => CIVICRM_SSL_KEY,
+    ));
 }
 
 /**
